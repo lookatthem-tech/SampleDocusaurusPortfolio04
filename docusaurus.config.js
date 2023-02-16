03bbb670ec4d743723f5918e49cc91d9a377d21e
lookatthem-tech <112528495+lookatthem-tech@users.noreply.github.com>
2023-02-16 17:47:21 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Documentation Portfolio',
-  tagline: 'by Ryan Rader',
+  tagline: 'by Ryan Rader01',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
