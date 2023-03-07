2e611edd1d65518b9e975455c22c717056d7d547
lookatthem-tech <112528495+lookatthem-tech@users.noreply.github.com>
2023-03-07 18:24:48 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   url: 'https://lookatthem-tech.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/SampleDocusaurusPortfolio04/docs',
+  baseUrl: '/SampleDocusaurusPortfolio04/',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   favicon: 'img/favicon.ico',  
