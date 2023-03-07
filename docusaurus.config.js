f835c2e6c6b92d30dac38d67f55fb796a5042d5a
lookatthem-tech <112528495+lookatthem-tech@users.noreply.github.com>
2023-03-07 18:34:28 -0500

    Revert "Update docusaurus.config.js"
    This reverts commit cc530ed4fbca9ab3c58ebcf6bc0ead7ff18e1ee3.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,6 @@ const config = {
   
   organizationName: 'lookatthem-tech', // Usually your GitHub org/user name.
   projectName: 'SampleDocusaurusPortfolio04', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
