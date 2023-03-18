e61c19bb90103c65d9052a27f541ec02250ff534
lookatthem-tech <112528495+lookatthem-tech@users.noreply.github.com>
2023-03-18 09:46:49 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            About me 😏
+            About me 😎
           </Link>
         </div>
       </div>
